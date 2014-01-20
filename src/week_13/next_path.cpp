dc9b49fb196b5b88a41ffe76944ecfa3d930b84d
Diego Ballesteros Villamizar <diegob@student.ethz.ch>
2014-01-20 15:43:00 +0100

    Next Path: 100

--- src/week_13/next_path.cpp
+++ src/week_13/next_path.cpp
@@ -2,24 +2,30 @@
 #include <iostream>
 #include <map>
 #include <queue>
-
+#include <limits>
+#include <list>
+#include <vector>
 
 using namespace std;
 using namespace boost;
 
 typedef adjacency_list<vecS, vecS, directedS> Graph;
 
-int distance_to_target(const Graph &network, int start, bool *visited_in_p, int t, int n){
+const int MAX = numeric_limits<int>::max();
+
+int distance_to_target(const Graph &network, int start, int t, int n, vector<int> &predators){
     if(start == t)
         return 0;
     bool *visited = new bool[n];
     int *distances = new int[n];
     for(int i = 0; i < n; ++i){
-        visited[i] = visited_in_p[i];
+        visited[i] = false;
+        predators.push_back(-1);
     }
     distances[start] = 0;
     queue<int> to_visit;
     to_visit.push(start);
+    visited[start] = true;
     while(!to_visit.empty()){
         int next = to_visit.front();
         to_visit.pop();
@@ -27,16 +33,18 @@ int distance_to_target(const Graph &network, int start, bool *visited_in_p, int 
         for(tie(ei, e_end) = out_edges(next, network); ei != e_end; ++ei){
             int next_next = target(*ei, network);
             if(next_next == t){
+                predators[next_next] = next;
                 return distances[next] + 1;
             }
             if(!visited[next_next]){
                 to_visit.push(next_next);
                 visited[next_next] = true;
                 distances[next_next] = distances[next] + 1;
+                predators[next_next] = next;
             }
         }
     }
-    return 10000000;
+    return -1;
 }
 
 int main(){
@@ -50,90 +58,41 @@ int main(){
         --s;
         --tar;
         Graph network(n);
-        Graph reverse_network(n);
         for(int j = 0; j < m; ++j){
             int a,b;
             cin >> a >> b;
             --a;
             --b;
-            if(a == b)
-                continue;
             add_edge(a,b, network);
-            add_edge(b,a, reverse_network);
         }
-        bool *visited = new bool[n];
-        for(int j = 0; j < n; ++j){
-            visited[j] = false;
-        }
-        queue<int> to_visit;
-        int *predecessors = new int[n];
-        int *distance_from_source = new int[n];
-        for(int j = 0; j < n; ++j){
-            visited[j] = false;
-            predecessors[j] = j;
-            distance_from_source[j] = 10000000;
-        }
-        visited[s] = true;
-        distance_from_source[s] = 0;
-        bool condition = true;
-        to_visit.push(s);
-        while(!to_visit.empty() && condition){
-            int new_vertex = to_visit.front();
-            to_visit.pop();
-            graph_traits<Graph>::out_edge_iterator ei, e_end;
-            for(tie(ei, e_end) = out_edges(new_vertex, network); ei != e_end; ++ei){
-                int target_v = target(*ei, network);
-                if(target_v == tar){
-                    condition = false;
-                    predecessors[target_v] = new_vertex;
-                    distance_from_source[target_v] = distance_from_source[new_vertex] + 1;
-                    break;
-                }
-                if(!visited[target_v]){
-                    to_visit.push(target_v);
-                    visited[target_v] = true;
-                    predecessors[target_v] = new_vertex;
-                    distance_from_source[target_v] = distance_from_source[new_vertex] + 1;
-                }
-            }
-        }
-        if(condition){
+        vector<int> shortestPredators;
+        int min_cost = distance_to_target(network, s, tar, n, shortestPredators);
+        if(min_cost < 0){
             cout << "no\n";
             continue;
         }
-        int *successors = new int[n];
-        for(int j = 0; j < n; ++j){
-            successors[j] = j;
+        vector<int> P(min_cost + 1);
+        P[min_cost] = tar;
+        int current = tar;
+        for(int j = min_cost - 1; j >= 0; --j){
+            P[j] = shortestPredators[current];
+            current = shortestPredators[current];
         }
-        int next_to_visit = tar;
-        while(next_to_visit != s){
-            successors[predecessors[next_to_visit]] = next_to_visit;
-            next_to_visit = predecessors[next_to_visit];
-        }
-        bool *visited_in_p = new bool[n];
-        for(int j = 0; j < n; ++j){
-            visited_in_p[j] = false;
-        }
-        next_to_visit = s;
-        int min_cost_2 = 1000000;
-        do{
-            visited_in_p[next_to_visit] = true;
+        int min_cost_2 = MAX;
+        for(int j = 0; j < min_cost + 1; ++j){
             graph_traits<Graph>::out_edge_iterator ei, e_end;
-            for(tie(ei, e_end) = out_edges(next_to_visit, network); ei != e_end; ++ei){
-                int possible_exit = target(*ei, network);
-                if(possible_exit != successors[next_to_visit]){
-                    int cost_from_there = distance_from_source[next_to_visit] + 1 + distance_to_target(network, possible_exit, visited_in_p, tar, n);
-                    if(cost_from_there < min_cost_2){
-                        min_cost_2 = cost_from_there;
-                    }
+            for(tie(ei, e_end) = out_edges(P[j], network); ei != e_end; ++ei){
+                if(j == min_cost || target(*ei, network) != P[j + 1]){
+                    vector<int> dummy;
+                    int another_cost = distance_to_target(network, target(*ei, network), tar, n, dummy);
+                    if(another_cost >= 0 && another_cost + 1 + j < min_cost_2)
+                        min_cost_2 = another_cost + 1 + j;
                 }
             }
-            next_to_visit = successors[next_to_visit];
-        } while (next_to_visit != tar);
-        if(min_cost_2 == 1000000){
-            cout << "no\n";
-        } else {
-            cout << min_cost_2 << "\n";
         }
+        if(min_cost_2 == MAX)
+            cout << "no\n";
+        else
+            cout << min_cost_2 << "\n";
     }
 }
