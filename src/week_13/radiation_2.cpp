dee10eeba33cfe46d5c96e4fde897f80bfacec9c
Diego Ballesteros Villamizar <diegob@student.ethz.ch>
2014-01-21 20:07:42 +0100

    Radiation II - 100!
    It's fucking 6000, yo!

--- src/week_13/radiation_2.cpp
+++ src/week_13/radiation_2.cpp
@@ -1,19 +1,33 @@
-#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Delaunay_triangulation_2.h>
 #include <cmath>
 #include <iostream>
 #include <vector>
+#include <set>
 
 using namespace std;
 
-typedef CGAL::Exact_predicates_exact_constructions_kernel K;
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 typedef CGAL::Delaunay_triangulation_2<K> Triangulation;
 typedef CGAL::Point_2<K> Point;
 typedef CGAL::Segment_2<K> Segment;
 typedef CGAL::Ray_2<K> Ray;
 typedef CGAL::Triangle_2<K> Triangle;
 
+class CircleOrdering {
+        Point p0, p1;
+    public:
+        CircleOrdering(const Point &a, const Point &b){
+            p0 = a;
+            p1 = b;
+        }
+        bool operator()(const Point &a, const Point &b){
+            return CGAL::ON_UNBOUNDED_SIDE == CGAL::side_of_bounded_circle(p0, p1, b, a);
+        }
+};
+
 int main(){
+    ios_base::sync_with_stdio(false);
     int t;
     cin >> t;
     for(int i = 0; i < t; ++i){
@@ -29,66 +43,81 @@ int main(){
             else
                 sick[j - m] = Point(x,y);
         }
-        Triangle realTriangle(healthy[0], healthy[1], healthy[2]);
-        Segment triangle[3] = {Segment(healthy[0], healthy[1]), Segment(healthy[1], healthy[2]), Segment(healthy[2], healthy[0])};
-        Triangulation goodcells;
-        goodcells.insert(healthy.begin(), healthy.end());
-        set<pair<K::FT, Point>, std::greater<pair<K::FT, Point> > > candidate_circles;
-        for(Triangulation::Finite_faces_iterator it = goodcells.finite_faces_begin(); it != goodcells.finite_faces_end(); ++it){
-            Point circle_center = goodcells.dual(it);
-            candidate_circles.insert(pair<K::FT, Point>(CGAL::squared_distance(circle_center, it->vertex(0)->point()),circle_center));
+        Triangulation good_cells;
+        good_cells.insert(healthy.begin(), healthy.end());
+        map< Triangulation::Face_handle, set<Point> > points_in_circum;
+        for(vector<Point>::iterator it = sick.begin(); it != sick.end(); ++it){
+            vector<Triangulation::Face_handle> face_collection;
+            good_cells.get_conflicts(*it, back_inserter(face_collection));
+            for(vector<Triangulation::Face_handle>::iterator it2 = face_collection.begin(); it2 != face_collection.end(); ++it2){
+                points_in_circum[*it2].insert(*it);
+            }
         }
-        for(Triangulation::Finite_edges_iterator it = goodcells.finite_edges_begin(); it != goodcells.finite_edges_end(); ++it){
-            CGAL::Object voronoiEdge = goodcells.dual(*it);
-            Segment delaunaySegment = goodcells.segment(*it);
-            if(const Segment *vSegment = CGAL::object_cast<Segment>(&voronoiEdge)){
-                if(CGAL::do_intersect(delaunaySegment, *vSegment)){
-                    Point candidate_center = CGAL::midpoint(delaunaySegment.source(), delaunaySegment.target());
-                    candidate_circles.insert(pair<K::FT, Point>(delaunaySegment.squared_length()/4, candidate_center));
-                } 
-                bool source_inside = !realTriangle.has_on_unbounded_side(vSegment->source());
-                bool target_inside = !realTriangle.has_on_unbounded_side(vSegment->target());
-                if((source_inside && !target_inside) || (!source_inside && target_inside)){
-                    for(int j = 0; j < 3; ++j){
-                        if(CGAL::do_intersect(triangle[j], *vSegment)){
-                            CGAL::Object intersect = CGAL::intersection(triangle[j], *vSegment);
-                            if(const Point *cross = CGAL::object_cast<Point>(&intersect)){
-                                Point test_point = goodcells.nearest_vertex(*cross)->point();
-                                K::FT radius = CGAL::squared_distance(*cross, test_point);
-                                candidate_circles.insert(pair<K::FT, Point>(radius, *cross));
-                            }
-                        }
+        int total_max = 0;
+        for(Triangulation::Finite_edges_iterator it = good_cells.finite_edges_begin(); it != good_cells.finite_edges_end(); ++it){
+            Triangulation::Face_handle f_one = it->first;
+            Triangulation::Face_handle f_two = it->first->neighbor(it->second);
+            vector<Point> in_one_not_in_two;
+            vector<Point> in_two_not_in_one;
+            set_difference(points_in_circum[f_one].begin(), points_in_circum[f_one].end(), points_in_circum[f_two].begin(), points_in_circum[f_two].end(), inserter(in_one_not_in_two, in_one_not_in_two.end()));
+            set_difference(points_in_circum[f_two].begin(), points_in_circum[f_two].end(), points_in_circum[f_one].begin(), points_in_circum[f_one].end(), inserter(in_two_not_in_one, in_two_not_in_one.end()));
+            vector<Point> in_both;
+            set_intersection(points_in_circum[f_two].begin(), points_in_circum[f_two].end(), points_in_circum[f_one].begin(), points_in_circum[f_one].end(), inserter(in_both, in_both.end()));
+            if(in_one_not_in_two.size() == 0 || in_two_not_in_one.size() == 0){
+                if(std::max(in_one_not_in_two.size(), in_two_not_in_one.size()) + in_both.size() > total_max){
+                    total_max = std::max(in_one_not_in_two.size(), in_two_not_in_one.size()) + in_both.size();
+                }
+                continue;
+            }
+            CircleOrdering sorter(f_one->vertex(f_one->cw(it->second))->point(), f_one->vertex(f_one->ccw(it->second))->point());
+            sort(in_one_not_in_two.begin(), in_one_not_in_two.end(), sorter);
+            sort(in_two_not_in_one.begin(), in_two_not_in_one.end(), sorter);
+            int max_count = std::max(in_one_not_in_two.size(), in_two_not_in_one.size()) + in_both.size();
+            int current_count = in_one_not_in_two.size() + in_both.size();
+            vector<Point>::iterator it3 = in_two_not_in_one.end() - 1;
+            for(vector<Point>::iterator it2 = in_one_not_in_two.begin(); it2 != in_one_not_in_two.end(); ++it2){
+                while(it3 != in_two_not_in_one.end()){
+                    if(CGAL::side_of_bounded_circle(f_one->vertex(f_one->cw(it->second))->point(), f_one->vertex(f_one->ccw(it->second))->point(), *it2, *it3) == CGAL::ON_BOUNDED_SIDE){
+                        ++current_count;
+                    } else {
+                        break;
+                    }
+                    if(it3 == in_two_not_in_one.begin()){
+                        it3 = in_two_not_in_one.end();
+                    } else{
+                        --it3;
                     }
                 }
-            } else if(const Ray *vRay = CGAL::object_cast<Ray>(&voronoiEdge)){
-                if(CGAL::do_intersect(delaunaySegment, *vRay)){
-                    Point candidate_center = CGAL::midpoint(delaunaySegment.source(), delaunaySegment.target());
-                    candidate_circles.insert(pair<K::FT, Point>(delaunaySegment.squared_length()/4, candidate_center));
-                } else {
-                    
+                if(current_count > max_count){
+                    max_count = current_count;
                 }
+                current_count--;
+            }
+            current_count = in_two_not_in_one.size() + in_both.size();
+            it3 = in_one_not_in_two.end() - 1;
+            for(vector<Point>::iterator it2 = in_two_not_in_one.begin(); it2 != in_two_not_in_one.end(); ++it2){
+                while(it3 != in_one_not_in_two.end()){
+                    if(CGAL::side_of_bounded_circle(f_one->vertex(f_one->cw(it->second))->point(), f_one->vertex(f_one->ccw(it->second))->point(), *it2, *it3) == CGAL::ON_BOUNDED_SIDE){
+                        ++current_count;
+                    } else {
+                        break;
+                    }
+                    if(it3 == in_one_not_in_two.begin()){
+                        it3 = in_one_not_in_two.end();
+                    } else{
+                        --it3;
+                    }
+                }
+                if(current_count > max_count){
+                    max_count = current_count;
+                }
+                current_count--;
+            }
+            if(max_count > total_max){
+                total_max = max_count;
             }
         }
-        int max = 0;
-        Point max_center;
-        K::FT max_radius;
-        for(set<pair<K::FT,Point> >::iterator it = candidate_circles.begin(); it != candidate_circles.end(); ++it){
-            //cout << "Center: (" << it->second.x() << "," << it->second.y() << ")\n";
-            //cout << "Radius: " << it->first << "\n";
-            int current = 0;
-            for(vector<Point>::iterator it2 = sick.begin(); it2 != sick.end(); ++it2){
-                if(CGAL::squared_distance(*it2, it->second) < it->first){
-                    ++current;
-                }
-            }
-            if(current > max){
-                max = current;
-                max_center = it->second;
-                max_radius = it->first;
-            }
-        }
-        cout << "Max center: " << max_center.x() << " " << max_center.y() << "\n";
-        cout << "Max radius: " << sqrt(CGAL::to_double(max_radius)) << "\n";
-        cout << max << "\n";
+        cout << total_max << "\n";
+
     }
 }
